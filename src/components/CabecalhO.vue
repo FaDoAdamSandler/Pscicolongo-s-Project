3a6c53a34d76cfcab62cd9c0a5d7c9374cbdaf4c
FaDoAdamSandler <99231788+FaDoAdamSandler@users.noreply.github.com>
2022-05-12 14:24:35 -0300

    Add files via upload

---
+++ src/components/CabecalhO.vue
@@ -0,0 +1,11 @@
+<script>
+export default {};
+</script>
+<template>
+  <header id="header">
+    <input class="textopesquisa" type="text" />
+    <button class ButtonPesquisa>Pesquisar</button>
+    <article class="cicle"></article>
+  </header>
+</template>
+<style></style>
---
+++ src/components/MenuLateral.vue
@@ -0,0 +1,14 @@
+<script>
+export default {};
+</script>
+<template>
+  <aside id="sidenav">
+    <article class="blocklateral"></article>
+    <article class="blocklateral"></article>
+    <article class="blocklateral"></article>
+    <article class="blocklateral"></article>
+    <article class="blocklateral"></article>
+    <article class="blocklateral"></article>
+  </aside>
+</template>
+<style></style>
